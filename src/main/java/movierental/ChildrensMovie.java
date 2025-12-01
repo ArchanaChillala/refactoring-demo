a5f7f056f4a551795f1f12a87f2b294355f5dff1
Archana Chillala <archanac@Archanas-MacBook-Pro.local>
2025-12-01 16:03:03 +0530

    Make Price Calculation more readable

--- src/main/java/movierental/ChildrensMovie.java
+++ src/main/java/movierental/ChildrensMovie.java
@@ -3,7 +3,7 @@ package movierental;
 public class ChildrensMovie extends Movie {
     
     private static final double BASE_CHARGE = 1.5;
-    private static final int FREE_DAYS = 3;
+    private static final int THRESHOLD_DAYS = 3;
     private static final double ADDITIONAL_CHARGE_PER_DAY = 1.5;
     
     public ChildrensMovie(String title) {
@@ -12,9 +12,20 @@ public class ChildrensMovie extends Movie {
     
     @Override
     public double getCharge(int daysRented) {
-        double amount = BASE_CHARGE;
-        if (daysRented > FREE_DAYS)
-            amount += (daysRented - FREE_DAYS) * ADDITIONAL_CHARGE_PER_DAY;
-        return amount;
+        if (exceedsThreshold(daysRented))
+            return chargeWithExtraDays(daysRented);
+        return BASE_CHARGE;
+    }
+
+    private static double chargeWithExtraDays(int daysRented) {
+        return BASE_CHARGE + extraCharge(daysRented);
+    }
+
+    private static double extraCharge(int daysRented) {
+        return (daysRented - THRESHOLD_DAYS) * ADDITIONAL_CHARGE_PER_DAY;
+    }
+
+    private static boolean exceedsThreshold(int daysRented) {
+        return daysRented > THRESHOLD_DAYS;
     }
 }
