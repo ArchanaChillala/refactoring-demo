e972dcac0a33454690a8231642b28cb429462bd2
Archana Chillala <archanac@Archanas-MacBook-Pro.local>
2025-12-02 09:28:22 +0530

    Eliminate If-else block in ItemFactory

--- src/main/java/gildedrose/ItemFactory.java
+++ src/main/java/gildedrose/ItemFactory.java
@@ -1,24 +1,28 @@
 package gildedrose;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
 public class ItemFactory {
     
     private static final String AGED_BRIE = "Aged Brie";
     private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
+    private static final Map<String, BiFunction<Integer, Integer, Item>> ITEM_CREATORS = createItemCreators();
+
+    private static Map<String, BiFunction<Integer, Integer, Item>> createItemCreators() {
+        Map<String, BiFunction<Integer, Integer, Item>> creators = new HashMap<>();
+        creators.put(AGED_BRIE, AgedBrie::new);
+        creators.put(BACKSTAGE_PASSES, BackstagePass::new);
+        creators.put(SULFURAS, Sulfuras::new);
+        return creators;
+    }
+
     public static Item createItem(String name, int sellIn, int quality) {
-        if (AGED_BRIE.equals(name)) {
-            return new AgedBrie(sellIn, quality);
-        }
-        
-        if (BACKSTAGE_PASSES.equals(name)) {
-            return new BackstagePass(sellIn, quality);
-        }
-        
-        if (SULFURAS.equals(name)) {
-            return new Sulfuras(sellIn, quality);
-        }
-        
-        return new Item(name, sellIn, quality);
+        return ITEM_CREATORS
+                .getOrDefault(name, (s, q) -> new Item(name, s, q))
+                .apply(sellIn, quality);
     }
 }
